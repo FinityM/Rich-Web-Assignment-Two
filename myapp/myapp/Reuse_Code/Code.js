919aa9e19d9c02b7bce5da0dd8679539a4353bb3
FinityM <B00125089@mytudublin.ie>
2021-12-16 15:36:09 +0000

    Separated validation and restored database connection back to scratch

---
+++ myapp/myapp/Reuse_Code/Code.js
@@ -0,0 +1,55 @@
+// Other email validator
+// var em = require('is-email');
+//
+// em.isEmail('is-email');
+//
+// var r1 = em.isEmail(email);
+//
+// console.log("email not valid" + r1);
+
+// SQL sanitisation
+// var sqlstring = require('sqlstring');
+//
+// var sqlusername = sqlstring.escape(username);
+// var sqlemail = sqlstring.escape(email);
+// var sqlpass = sqlstring.escape(password);
+//
+// console.log("es SQL attack username: " + sqlusername);
+// console.log("es SQL attack email: " + sqlemail);
+// console.log("es SQL attack pass: " + sqlpass);
+
+// Validate the email
+// var result = evalidator.validate(email);
+
+// Validate the username
+// const has = require('has-value');
+// var hasValue = has(username);
+// let n = username.search("^[a-zA-Z0-9_.-]*$");
+// console.log(n);
+
+// Validate the password
+// const bcrypt = require('bcrypt');
+// const saltRounds = 15;
+
+// if (result == false || result == null || result == '' || !result) {
+//     alert('Email is empty\n');
+//     errorMessage += 'Email not valid <br>';
+// }
+//
+// if (hasValue == false) {
+//     alert('Username is empty\n');
+//     errorMessage += 'Username left empty <br>';
+// }
+
+// Username sanitisation
+// var xss = require("xss");
+// var cleanedUsername = xss(username);
+// console.log(cleanedUsername);
+
+// Hash and print the hashed password to the console
+// bcrypt.hash(password, saltRounds, function (err, hash) {
+//
+//     // Store hash in password
+//     console.log(hash);
+//
+// });
--- myapp/myapp/app.js
+++ myapp/myapp/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var validator = require('validator');
-var evalidator = require('email-validator');
+
 
 var indexRouter = require('./routes/index');
 // import
@@ -13,10 +13,6 @@ var indexRouter2 = require('./routes/index2');
 
 
 var usersRouter = require('./routes/users');
-const mysql = require("mysql");
-const has = require("has-value");
-const bcrypt = require("bcrypt");
-const sqlstring = require("sqlstring");
 
 var app = express();
 
@@ -45,7 +41,6 @@ app.get('/login', function (req, res) {
 
 });
 
-
 app.post('/login', function (req, res) {
 
     // catch the username that was sent to us from the jQuery POST on the index.ejs page
@@ -53,92 +48,31 @@ app.post('/login', function (req, res) {
     var password = req.body.password;
     var email = req.body.email;
 
-    // Other email validator
-    // var em = require('is-email');
-    //
-    // em.isEmail('is-email');
-    //
-    // var r1 = em.isEmail(email);
-    //
-    // console.log("email not valid" + r1);
-
-    var errorMessage = '';
-
-    // SQL sanitisation
-    var sqlstring = require('sqlstring');
-
-    var sqlusername = sqlstring.escape(username);
-    var sqlemail = sqlstring.escape(email);
-    var sqlpass = sqlstring.escape(password);
-
-    console.log("es SQL attack username: " + sqlusername);
-    console.log("es SQL attack email: " + sqlemail);
-    console.log("es SQL attack pass: " + sqlpass);
-
-    // Validate the email
-    var result = evalidator.validate(email);
-
-    // Validate the username
-    const has = require('has-value');
-    var hasValue = has(username);
-    let n = username.search("^[a-zA-Z0-9_.-]*$");
-    console.log(n);
-
-    // Validate the password
-    const bcrypt = require('bcrypt');
-    const saltRounds = 15;
-
-    if (result == false || result == null || result == '' || !result) {
-        alert('Email is empty\n');
-        errorMessage += 'Email not valid <br>';
-    }
-
-    if (hasValue == false) {
-        alert('Username is empty\n');
-        errorMessage += 'Username left empty <br>';
-    }
-
-    // Username sanitisation
-    var xss = require("xss");
-    var cleanedUsername = xss(username);
-    console.log(cleanedUsername);
-
     // Print it out to the NodeJS console just to see if we got the variable.
-    // console.log("User name = " + username);
-    // console.log("Password = " + password);
-    // console.log("Email = " + email);
+    console.log("User name = " + username);
+    console.log("Password = " + password);
+    console.log("Email = " + email);
 
-    if (errorMessage.length == 0) {
-        // Remember to check what database you are connecting to and if the
-        // values are correct.
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'assignment_two'
-        });
+    // Remember to check what database you are connecting to and if the
+    // values are correct.
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'assignment_two'
+    });
 
-        // Hash and print the hashed password to the console
-        bcrypt.hash(sqlpass, saltRounds, function (err, hash) {
-            connection.connect();
+    connection.connect();
 
-            // This is the actual SQL query part
-            connection.query("INSERT INTO `assignment_two`.`login` (`username`, `password`, `email`) VALUES ('" + sqlusername + "', '" + hash + "', '" + sqlemail + "');", function (error, results, fields) {
-                if (error) {
-                    res.send(error.code);
-                } else res.send('received');
-            });
+    // This is the actual SQL query part
+    connection.query("INSERT INTO `assignment_two`.`login` (`username`, `password`, `email`) VALUES ('" + username + "', '" + password + "', '" + email + "');", function (error, results, fields) {
+        if (error) throw error;
+    });
 
-            // Store hash in password
-            console.log(hash);
-            connection.end();
+    connection.end();
 
-        });
-    } else {
-        res.send(errorMessage);
-    }
-
+    res.send("received");
 });
 
 
@@ -157,5 +91,6 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
 
 module.exports = app;
